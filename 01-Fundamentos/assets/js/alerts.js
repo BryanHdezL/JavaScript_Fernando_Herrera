ab463f2067f3f35119a17c27d5055feee9187c7f
BryanHdezL <bryan.hdezl2407@hotmail.com>
2025-03-30 01:07:24 -0600

    feat: javaScript introduction and console

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,10 @@
+// Alerta con mensaje informativo
+alert('Hola mundo!')
+
+// Alerta con input
+let response = prompt('¿Cual es tu nombre?')
+console.log('Name: ' + '****' + response + '****') // ''
+
+// Alerta con botones de confirmación
+const select = confirm('¿Estas seguro?')
+console.log('Select: ' + select)
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,37 @@
+// Hola mundo en JS
+
+// console.log('Hola mundo!')
+
+// Variables en JS
+
+const c = 10 // Forma para declarar constantes
+let a = 10 // Forma recomendada de declarar variables
+var b = 10 // Forma antigua utilizada para declarar variables
+
+// console.log('a:', a)
+// console.log('b: ' + b)
+// console.log(`c: ${c}`)
+// console.log({ a, b, c })
+// console.table({ a, b, c })
+
+/****************************************************************************
+ 
+                        -----------------------
+                                NOTAS
+                        -----------------------
+
+- Diferencias entre VAR y LET
+    - var:  Funciona dentro de toda la función donde se declara, permite
+            volver a declararse, funcionando dentro del objeto window.
+    - let:  Solo existe dentro del scope donde se define, no permite
+            redeclaración y da error si se usa antes de declararse.
+- ECMAScript
+    ECMA es el estándar que rige las reglas del lenguaje de programación de 
+    JavaScript cada año se publica una version nueva del ECMA, con pequeñas
+    reglas o funcionalidades añadidas.
+
+- Polyfill
+    Es un código que provee el funcionamiento de una nueva característica 
+    de JavaScript (ES6), en versiones viejas como ES5.
+
+****************************************************************************/
